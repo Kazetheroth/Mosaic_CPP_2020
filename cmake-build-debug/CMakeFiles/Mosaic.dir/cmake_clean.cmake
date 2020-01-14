5471cc606235aeded511deccc7537017b50875e9
HeoliaJdR <admin@heolia.eu>
2020-01-14 11:44:12 +0100

    first commit

---
+++ cmake-build-debug/CMakeFiles/Mosaic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mosaic.dir/main.cpp.obj"
+  "Mosaic.exe"
+  "Mosaic.exe.manifest"
+  "Mosaic.lib"
+  "Mosaic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mosaic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
